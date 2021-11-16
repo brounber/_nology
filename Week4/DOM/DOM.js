1377434a7962f4cf95c2859af2d77a7fe8746a59
Raf <raf@brounber.com>
2021-11-16 12:33:40 +1100

    update

---
+++ Week4/DOM/DOM.js
@@ -0,0 +1,14 @@
+//1. create a button that allow 
+
+
+
+// selecting Single elemnet 
+const inputTask = document.querySelector("#taskInput");
+const main = document.querySelector("#main");
+const addBtn = document.querySelector("#addTaskBtn");
+
+// slecting multiple elements
+
+const cantainers = document.querySelector(".container");
+const div = document.querySelector("div");
+
