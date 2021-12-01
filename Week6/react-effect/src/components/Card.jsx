dd888b549bec3e9872c0d34ac3b62157fe0b31e5
Raf <raf@brounber.com>
2021-12-01 11:05:33 +1100

    update

---
+++ Week6/react-effect/src/components/Card.jsx
@@ -0,0 +1,19 @@
+
+const CATFACTS_URL = "https://catfact.ninja/facts";
+
+const card = ()=> {
+  useEffect() {
+    
+
+  }
+}
+
+
+export default Card;
+
+// const getBreeds = async (pageNumber) => {
+//   const response = await fetch(`${CATFACTS_URL}breeds?page=${pageNumber}`);
+//   const data = await response.json();
+//   console.log(data);
+//   return data.data;
+// };
---
+++ Week6/react-effect/src/containers/Home.jsx
@@ -0,0 +1,15 @@
+import Card from "../../components/Card";
+import styles from "./Home.module.scss";
+
+const Home = () => {
+    return (
+        <div className="app">
+            <h1>Cat's Funfacts</h1>
+            <Card />
+            <Card />
+            
+        </div>
+    );
+};
+
+export default Home;
