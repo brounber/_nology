a8fb2db987d2ba9b137925a11e729bdce44765d5
Raf <raf@brounber.com>
2022-01-02 20:50:13 +1100

    update

---
+++ Java/Calculator/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        String operation;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please Enter The First Number");
+        num1 = input.nextInt();
+
+        System.out.println("Please Enter The Second Number");
+        num2 = input.nextInt();
+
+        Scanner op = new Scanner (System.in);
+
+        System.out.println("Please Enter The Operation");
+        operation = op.next();
+
+        if (operation.equals("+"))
+        {
+            System.out.println("Your Answer is "+(num1 + num2));
+        }
+        else if (operation.equals("-"))
+        {
+            System.out.println("Your Answer is "+(num1 - num2));
+        }
+        else if (operation.equals("*"))
+        {
+            System.out.println("Your Answer is "+(num1 * num2));
+        }
+        else if (operation.equals("/"))
+        {
+            System.out.println("Your Answer is "+(num1 / num2));
+        }
+    }
+
+}
---
+++ portfolio-you-tube-project-main/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import footerImg from "./img/logo (1).png";
+function Footer() {
+  return (
+    <div>
+      <img src={footerImg} alt="" className="footer__img pointer" />
+      
+    </div>
+  );
+}
+
+export default Footer;
