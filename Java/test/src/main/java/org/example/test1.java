791b4165489692d6e149df62a66ac841347f3194
Raf <raf@brounber.com>
2022-05-09 19:28:03 +1000

    update

--- Java/test/src/main/java/org/example/test1.java
+++ Java/test/src/main/java/org/example/test1.java
@@ -1,2 +1,8 @@
-package org.example;public class test1 {
+package org.example;
+
+public class test1 {
+    public static int power(int voltage, float current) {
+        //return voltage*current;
+        return 5;
+    }
 }
